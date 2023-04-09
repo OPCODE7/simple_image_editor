07c93651dce92fb1d0982d58c771c353e9b412b6
Op Code <suarezdarlin41@gmail.com>
2023-04-08 21:47:14 -0600

    Load of images

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const d= document;
-const $selectImage= d.querySelector("#file");
+const d = document;
+const $previewImage= d.querySelector(".editor__picture");
+const $buttonGetFile= d.querySelector(".editor__file");
+const $buttonCloseImage= d.querySelector(".editor__close__image"); 
 
-d.addEventListener("click", e => {
-    if(e.target===$selectImage){
-        
+
+d.addEventListener("change", e => {
+    if (e.target.matches("#file")) {
+        previewFile($previewImage,e.target);
     }
 });
+
+d.addEventListener("click", e => {
+    if(e.target.matches(".editor__close__image")){
+        $previewImage.classList.add("d-none");
+        $buttonGetFile.classList.remove("d-none");
+        $buttonCloseImage.style.display="none";
+    }
+});
+
+function previewFile(preview,input) {
+    const file= input.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener("load", () => {
+        preview.src = reader.result;
+        $buttonGetFile.classList.add("d-none");
+        preview.classList.remove("d-none");
+        $buttonCloseImage.style.display="block";
+        
+    },false
+    );
+    
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
